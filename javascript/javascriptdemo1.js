b5b4e9d6cf86487e7d3d230632b7d7da2d94c414
hetalpatel29 <80544632+hetalpatel29@users.noreply.github.com>
2021-05-07 17:15:23 +0530

    my practice

---
+++ javascript/javascriptdemo1.js
@@ -0,0 +1,10 @@
+function changeimg1() {
+    document.getElementById("image1").src = ("images/IMG_5565-1024x683.jpg");
+    document.getElementById("image1").style.borderRadius = "50%";
+}
+
+function changeimg2() {
+    document.getElementById("image1").src = ("images/images-3.jpg");
+    document.getElementById("image1").style.borderRadius = " 70px 70px 70px 70px";
+
+}
